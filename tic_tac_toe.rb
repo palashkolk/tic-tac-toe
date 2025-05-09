8db4d341bf2173b4dd0fa7fd54736c9a55c0d41e
palashkolk <203395566+palashkolk@users.noreply.github.com>
2025-05-09 17:20:25 +0530

    Adds game drawn condition
    Only winner case included before.

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -13,7 +13,7 @@ def game_start_message
 end
 
 def player_move(player_number, move_tracker)
-  puts "Enter player#{player_number} position"
+  print "Enter player#{player_number} position: "
   player_position = gets.chomp.to_i
   row_idx = (player_position - 1) / 3
   col_idx = (player_position - 1) % 3
@@ -21,30 +21,39 @@ def player_move(player_number, move_tracker)
 end
 
 def game_tracker(move_tracker)
-  first_row = move_tracker[0][0] == move_tracker[0][1] && move_tracker[0][1] == move_tracker[0][2]
-  second_row = move_tracker[1][0] == move_tracker[1][1] && move_tracker[1][1] == move_tracker[1][2]
-  third_row = move_tracker[2][0] == move_tracker[2][1] && move_tracker[2][1] == move_tracker[2][2]
-  first_col = move_tracker[0][0] == move_tracker[1][0] && move_tracker[1][0] == move_tracker[2][0]
-  second_col = move_tracker[0][1] == move_tracker[1][1] && move_tracker[1][1] == move_tracker[2][1]
-  third_col = move_tracker[0][2] == move_tracker[1][2] && move_tracker[1][2] == move_tracker[2][2]
-  cross1 = move_tracker[0][0] == move_tracker[1][1] && move_tracker[1][1] == move_tracker[2][2]
-  cross2 = move_tracker[0][2] == move_tracker[1][1] && move_tracker[1][1] == move_tracker[2][0]
-  [first_row, second_row, third_row, first_col, second_col, third_col, cross1, cross2]
+  board_rows_cols = [move_tracker, move_tracker.transpose, [(0..2).map { |i| move_tracker[i][i] }, (0..2).map do |i|
+    move_tracker[i][2 - i]
+  end]].flatten(1)
+
+  game_winner = false
+  game_draw = false
+
+  board_rows_cols.each do |array|
+    game_winner = array.all?('X') || array.all?('0')
+    next unless game_winner
+
+    break
+  end
+  game_draw = move_tracker.flatten(1).all? { |item| %w[X 0].include?(item) } if game_winner == false
+  [game_winner, game_draw]
 end
 
 player = [1, 2]
 winner_declared = false
+game_drawn = false
 
 game_start_message
 display_board(move_tracker)
-until winner_declared
+
+until winner_declared or game_drawn
   player.each do |player|
     player_move(player, move_tracker)
     display_board(move_tracker)
-    winner_declared = true if game_tracker(move_tracker).include?(true)
-    if winner_declared == true
-      puts "player#{player} wins!"
-      break
-    end
+    winner_declared, game_drawn = game_tracker(move_tracker)
+    next unless winner_declared or game_drawn
+
+    puts "player#{player} wins!" if winner_declared
+    puts 'game drawn!' if game_drawn
+    break
   end
 end
